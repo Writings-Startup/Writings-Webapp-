5f54e0f5a629af73a2ff1eb637abc33ed38fb93c
Adrid Sadanand p <adridsadanand0001@gmail.com>
2021-06-29 19:43:01 +0530

    left side nav bar

--- src/Components/LeftsideBox.js
+++ src/Components/LeftsideBox.js
@@ -2,9 +2,30 @@ import React from 'react'
 import "../Css/LeftsideBox.css"
 function LeftsideBox() {
     return (
-        <div className="test">
-            <h1>Left side Box</h1>
-        </div>
+        <div className="body">
+        
+        <div className="nil">
+                <h4 className="nil2"></h4>
+            </div>  
+            <div className="content-box">
+                <h4 className="content">Feeds</h4>
+            </div>  
+            <div className="content-box">
+                <h4 className="content">Read List</h4>
+            </div>  
+            <div className="content-box">
+                <h4 className="content">Popular</h4>
+            </div>
+            <div className="content-box">
+                 <h4 className="content">Trend</h4>
+            </div>
+            <div className="content-box">
+                 <h4 className="content">Events</h4>
+            </div>
+        
+
+          </div>
+        
     )
 }
 
