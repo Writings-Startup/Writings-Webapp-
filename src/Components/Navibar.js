66b8cdce43b5f150869b62a96cd5ae3672c31a3f
akshaynambiarpv <akshaynambiar123>
2021-06-29 20:32:13 +0530

    navbar created

--- src/Components/Navibar.js
+++ src/Components/Navibar.js
@@ -2,9 +2,26 @@ import React from 'react'
 import "../Css/Navibar.css"
 function Navibar() {
     return (
-        <div className="testnavi">
-            <h1>Navibar</h1>
+        <div className="navbar">
+        <h2>logoname</h2>
+        <div className="leftSide">
+          <div className="linkLeft">
+            <a href="/">Home</a>
+            <a href="/">Notifications</a>
+          </div>
         </div>
+        <div className="middle">
+          <input type="text" placeholder="Search"></input>
+          <button>search</button>
+        </div>
+        <div className="rightSide">
+          <div className="linkRight">
+            <a href="/">Profile</a>
+            <a href="/"><button >Create</button></a>
+            <a href="/">Settings</a>
+          </div>
+        </div>
+      </div>
     )
 }
 
