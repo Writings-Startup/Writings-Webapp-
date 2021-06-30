49a58c0843643f61c991c00173417ca127fd5ec3
akshaynambiarpv <akshaynambiar123>
2021-06-30 22:54:53 +0530

    navbar edited to responsive

--- src/Components/Navibar.js
+++ src/Components/Navibar.js
@@ -2,26 +2,24 @@ import React from 'react'
 import "../Css/Navibar.css"
 function Navibar() {
     return (
-        <div className="navbar">
-        <h2>logoname</h2>
-        <div className="leftSide">
-          <div className="linkLeft">
-            <a href="/">Home</a>
-            <a href="/">Notifications</a>
-          </div>
-        </div>
-        <div className="middle">
-          <input type="text" placeholder="Search"></input>
-          <button>search</button>
-        </div>
-        <div className="rightSide">
-          <div className="linkRight">
-            <a href="/">Profile</a>
-            <a href="/"><button >Create</button></a>
-            <a href="/">Settings</a>
-          </div>
-        </div>
-      </div>
+      <div className="nav">
+      <div className="logo">Logoname</div>
+      <form>
+        <input type="text" placeholder="Search"></input>
+      </form>
+      <input type="checkbox" id="click"></input>
+      <label for="click" className="menu-btn">
+        <i class="fa fa-bars"></i>
+      </label>
+      <ul >
+        <li><a className="active" href="#">Home</a></li>
+        <li><a href="#">Notification</a></li>
+        <li><a href="#">Profile</a></li>
+        <li><button className="create-btn">Create</button></li>
+        <li><a href="#">Settings</a></li>
+      </ul>
+    </div>
+    
     )
 }
 
