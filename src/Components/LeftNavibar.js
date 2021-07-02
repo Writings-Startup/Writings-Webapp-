4cb26be67d5c5753f8f79819cbbbf13d1e992f2e
basithc <c96basith@gmail.com>
2021-07-03 04:08:11 +0530

    icons

--- src/Components/LeftNavibar.js
+++ src/Components/LeftNavibar.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import "../Css/LeftNavibar.css"
+import { IconContext } from 'react-icons';
+import { MdRssFeed, MdEvent } from "react-icons/md";
+import { AiOutlineRead, AiOutlineFire } from "react-icons/ai";
+import { GiAlliedStar } from "react-icons/gi";
 function LeftNavibar() {
     return (
         <div className="body">
@@ -8,19 +12,23 @@ function LeftNavibar() {
                 
         </div>
             <div className="content-box">
-                <h4 className="content">Feeds</h4>
+
+                
+
+                
+                <h4 className="content"> <MdRssFeed size= "30px" /> Feeds</h4>
             </div>  
             <div className="content-box">
-                <h4 className="content">Read List</h4>
+                <h4 className="content"><AiOutlineRead size= "30px" />Read List</h4>
             </div>  
             <div className="content-box">
-                <h4 className="content">Popular</h4>
+                <h4 className="content"><GiAlliedStar size= "30px" />Popular</h4>
             </div>
             <div className="content-box">
-                 <h4 className="content">Trend</h4>
+                 <h4 className="content"><AiOutlineFire size= "30px" />Trend</h4>
             </div>
             <div className="content-box">
-                 <h4 className="content">Events</h4>
+                 <h4 className="content"><MdEvent size= "30px" />Events</h4>
             </div>
         
 
