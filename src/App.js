50e50367ef5695b6d29448c57f78051cae382e88
akshaynambiarpv <akshaynambiar123>
2021-07-11 20:47:43 +0530

    routing edited

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./Pages/Home";
 import "./Css/App.css"
+import Navibar from './Components/Navibar';
 
 function App() {
   return (
     <div className="App">
-     <Home/>
+     <Navibar/>
      
     </div>
   );
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import Navibar from '../Components/Navibar'
+//import Navibar from '../Components/Navibar'
 import LeftNavibar from "../Components/LeftNavibar"
-import Newsfeed from "../Components/Newsfeed"
+//import Newsfeed from "../Components/Newsfeed"
 import "../Css/Home.css"
 
 function Home() {
     return (
         
         <div>
-            <Navibar/>
+            
             <LeftNavibar/>
-            <Newsfeed/>
+            
         </div>
 
        
