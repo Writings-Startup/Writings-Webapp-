00c7f5623dd03cdaf7c99d26c09e3065312db204
akshaynambiarpv <akshaynambiar123>
2021-07-24 20:53:39 +0530

    update

--- src/Components/CreatePage.js
+++ src/Components/CreatePage.js
@@ -4,9 +4,7 @@ import '../Css/create.css'
 function CreatePage() {
     return (
         <div className="Create">
-            <h1 className="create-hr">
-                Create page 
-            </h1>
+            <h1 className="create-hr"> Create page </h1>
         </div>
     )
 }
--- src/Pages/Create.js
+++ src/Pages/Create.js
@@ -6,12 +6,7 @@ function Create() {
     return (
         <div>
             <LeftNavibar/>
-            
-
-                <CreatePage/>
-
-       
-            
+            <CreatePage/>
         </div>
     )
 }
