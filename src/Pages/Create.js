9bd33ceb2c4413fcdd3c51c635a18ada940adf21
Adrid Sadanand p <adridsadanand0001@gmail.com>
2021-07-27 02:39:56 +0530

    create page bug

--- src/Pages/Create.js
+++ src/Pages/Create.js
@@ -5,7 +5,12 @@ import LeftNavibar from '../Components/LeftNavibar'
 function Create() {
     return (
         <div>
-            <LeftNavibar/>
+             <LeftNavibar
+             icon1="fa fa-rss" colone="Feeds"
+             icon2="fa fa-users" coltwo="magazine"
+             icon3="fa fa-free-code-camp" colthree="Booming"
+             icon4="fa fa-book" colfour="Guide"
+             icon5="fa fa-flag-checkered" colfive="Events"/>
             <CreatePage/>
         </div>
     )
--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import "../Css/Profile.css"
-
+import LeftNavibar from "../Components/LeftNavibar"
+import ProfilePage from '../Components/ProfilePage'
 function Profile() {
     return (
         <div>
-            profile page
+         <LeftNavibar
+             icon1="fa fa-rss" colone="Feeds"
+             icon2="fa fa-users" coltwo="magazine"
+             icon3="fa fa-free-code-camp" colthree="Booming"
+             icon4="fa fa-book" colfour="Guide"
+             icon5="fa fa-flag-checkered" colfive="Events"/>  
+            <ProfilePage/>
+
         </div>
     )
 }
