585543f8acf82d7e6991aa9d85c18339e387fe21
Adrid Sadanand p <adridsadanand0001@gmail.com>
2021-07-27 01:21:47 +0530

    leftnav bug

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,12 +3,18 @@ import Feeds from '../Components/Feeds/Feeds'
 import LeftNavibar from "../Components/LeftNavibar"
 import "../Css/Home.css"
 
+
 function Home() {
     return (
         
         <div className="home-main">
             
-           <LeftNavibar name="hai" second="hello"/>
+            <LeftNavibar
+             icon1="fa fa-rss" colone="Feeds"
+             icon2="fa fa-users" coltwo="magazine"
+             icon3="fa fa-free-code-camp" colthree="Booming"
+             icon4="fa fa-book" colfour="Guide"
+             icon5="fa fa-flag-checkered" colfive="Events"/>
            
         <div className="feedss">
 
