a11feca38f9da8f2ba211bc2880bb37c7ad3b522
akshaynambiarpv <akshaynambiar123>
2021-07-27 00:12:05 +0530

    added some images

--- src/Components/Feeds/Feeds.js
+++ src/Components/Feeds/Feeds.js
@@ -6,7 +6,11 @@ function Feeds() {
         {title: "Learning From The Sea" ,body:"53K views  •  2 weeks ago" ,author:" Andy William" , feedpic:"/Assets/images/Group 369.jpg", profilepic:"/Assets/profile/image 10.jpg",id:1},
         {title: "Ethics is nothing else than reverence for life " ,body:"5K views  •  3 weeks ago" ,author:"Johny Wise" ,feedpic:"/Assets/images/image 7.jpg",profilepic:"/Assets/profile/image 12.jpg", id:2},
         {title: "Blacksmiths with Determination " ,body:"3K views  •  4 weeks ago" ,author:"Budi Hakim" ,feedpic:"/Assets/images/image 6.jpg",profilepic:"/Assets/profile/image 11.jpg", id:3},
-        {title: "what's meant to be will always find a way" ,body:"503K views  •  5 weeks ago" ,author:" Wijaya Abadi" ,feedpic:"/Assets/images/image 19.jpg",profilepic:"/Assets/profile/image 13.jpg", id:4}
+        {title: "what's meant to be will always find a way" ,body:"503K views  •  5 weeks ago" ,author:" Wijaya Abadi" ,feedpic:"/Assets/images/image 19.jpg",profilepic:"/Assets/profile/image 13.jpg", id:4},
+        {title: "Tempor deserunt Sunt Qui" ,body:"503K views  •  November 3, 2014" ,author:" Sirius Black" ,feedpic:"/Assets/images/Rectangle 2.jpg",profilepic:"/Assets/profile/siriusblack.jpg", id:5},
+        {title: " Tara Sparling’s" ,body:"503K views  •  December 3, 2014" ,author:" Emily Brown" ,feedpic:"/Assets/images/nothumbnail.jpg",profilepic:"/Assets/profile/emilybrown.jpg", id:6},
+        {title: "Bay City Tribune" ,body:"503K views  •  January 3, 2013" ,author:" June" ,feedpic:"/Assets/images/nothumbnail.jpg",profilepic:"/Assets/profile/june.jpg", id:7},
+        {title: "Jak-Jaw Racing" ,body:"503K views  •  November 3, 2013" ,author:" Angelina Jolie" ,feedpic:"/Assets/images/nothumbnail.jpg",profilepic:"/Assets/profile/angelinajolie.jpg", id:8}
     ]);
 
 
