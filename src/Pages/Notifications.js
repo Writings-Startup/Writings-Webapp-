2853f7c4a10db5f3d4d9d852736b95f8e8e7c763
basithc <c96basith@gmail.com>
2021-08-01 23:17:55 +0530

    notification

--- src/Pages/Notifications.js
+++ src/Pages/Notifications.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import "../Css/Profile.css"
+import LeftNavibar from "../Components/LeftNavibar"
+import NotificationsPage from '../Components/NotificationsPage'
 
 function Notifications() {
     return (
         <div>
-            notification page
+             <LeftNavibar
+             icon1="fa fa-rss" colone="Feeds"
+             icon2="fa fa-users" coltwo="magazine"
+             icon3="fa fa-free-code-camp" colthree="Booming"
+             icon4="fa fa-book" colfour="Guide"
+             icon5="fa fa-flag-checkered" colfive="Events"/>  
+             <NotificationsPage/>
         </div>
     )
 }
