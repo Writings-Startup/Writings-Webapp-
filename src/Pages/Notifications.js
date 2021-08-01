0a0e6591a826a7b77194eced7b62ab972e57b9a7
Adrid Sadanand p <adridsadanand0001@gmail.com>
2021-08-01 23:49:32 +0530

    notification bugupdates

--- src/Pages/Notifications.js
+++ src/Pages/Notifications.js
@@ -1,16 +1,15 @@
 import React from 'react'
 
 import LeftNavbarmain from '../Components/LeftNavibar/LeftNavbarmain'
+import NotificationsPage from '../Components/NotificationsPage'
 
 
  
 function Notifications() {
     return (
         <div>
-
-            notification page
             <LeftNavbarmain/>
- 
+              <NotificationsPage/>
         </div>
     )
 }
